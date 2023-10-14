0ee28a201177a5f119b8c1b752950d1268301af6
NalinHong <66056044@kmitl.ac.th>
2023-10-15 02:45:49 +0700

    fixed == ex23

--- src/ex23.py
+++ src/ex23.py
@@ -22,7 +22,22 @@ def bottles_of_beer(param):
         str: The repeated lyrics of bottles of beer on the wall.
     """
     # TODO : complete this
-    pass
+    result = ''
+    if param != 1:
+        result += str(param) + ' bottles of beer on the wall,\n'
+        result += str(param) + ' bottles of beer.\n'
+        result += 'Take one down, pass it around,\n'
+        if (param - 1) == 1:
+            result += '1 bottle of beer on the wall.\n'
+        else:
+            result += str(param - 1) + ' bottles of beer on the wall.\n'
+    else:
+        result += '1 bottle of beer on the wall,\n'
+        result += '1 bottle of beer.\n'
+        result += 'Take one down, pass it around,\n'
+        result += 'No more bottles of beer on the wall!\n'
+    return result
+
 
 
 def loop_bottles_of_bears(bottle):
