576cb4df75569f6ba40333afdb792caae4924928
NalinHong <66056044@kmitl.ac.th>
2023-10-15 01:58:44 +0700

    fixed == ex16

--- src/ex16.py
+++ src/ex16.py
@@ -3,7 +3,7 @@ Exercise 16
 """
 
 
-def mode(params):
+def mode(num_list):
     """
     Calculate the mode of a list of numbers.
 
@@ -13,4 +13,17 @@ def mode(params):
     Returns:
     - int or None: The mode of the list, or None if the list is empty.
     """
-    pass
+    input_len = len(num_list)
+    if input_len == 0:
+        return None
+    max_count = 0
+    element_having_max_freq = 0
+    for i in range(0, input_len):
+        count = 0
+        for j in range(0,input_len):
+            if num_list[i] == num_list[j]:
+                count += 1
+        if count> max_count:
+            max_count=count
+            element_having_max_freq = num_list[i]
+    return element_having_max_freq
